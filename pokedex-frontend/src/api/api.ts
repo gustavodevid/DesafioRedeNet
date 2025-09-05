13289c6bbe8c11cbc630ba4e4dbfdb967348619f
Gusta <gustavo.cesariosilva15@gmail.com>
2025-09-05 10:03:19 -0300

    Feat: Deploy Vercel e Render

--- pokedex-frontend/src/api/api.ts
+++ pokedex-frontend/src/api/api.ts
@@ -9,9 +9,13 @@
 
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL; 
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${API_URL}/api`,
 });
+
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
